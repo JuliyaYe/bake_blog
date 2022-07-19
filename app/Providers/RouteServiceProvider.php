ccb6e3abc9f195e23a02cf4c5dc23faa8cdc3840
Yerohovets Yuliya <esprostoes@gmail.com>
2022-07-19 12:44:33 +0500

    Add admin panel

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,8 +29,17 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('admin')
+                ->prefix('admin')
+                ->name('admin.')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
+
+        
     }
 
     /**
